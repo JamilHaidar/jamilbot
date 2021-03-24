933cd07686598cece126eea2fc5c4a128185924d
Zingerella <jamil.haidarahmad@outlook.com>
2021-03-24 06:24:01 +0200

    Attempted environ vars

--- app/main.py
+++ app/main.py
@@ -7,4 +7,3 @@ app = Flask(__name__)
 def index():
     user = {'username': 'Miguel'}
     return render_template('index.html'),200
-import jamilbot
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
 from app.main import app
   
 if __name__ == "__main__":
+        print('In wsgi')
         app.run()
+        print('In wsgi: ran flask')
+        import jamilbot
+        print('In wsgi: imported jamilbot')
