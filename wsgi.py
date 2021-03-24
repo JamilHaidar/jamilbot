b70ced28efedf1cbbad1a800b10b3616fb4e84bb
Zingerella <jamil.haidarahmad@outlook.com>
2021-03-24 06:35:56 +0200

    Running flask and bot in parallel using threading

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,11 @@
 from app.main import app
-  
+from threading import Thread
 if __name__ == "__main__":
         print('In wsgi')
-        app.run()
+        flasktask = Thread(target=app.run).start()
         print('In wsgi: ran flask')
         import jamilbot
-        print('In wsgi: imported jamilbot')
+        print('In wsgi: Import done. running..')
+        jamilbot.run()
+        print('Done running')
+        flasktask.join()
