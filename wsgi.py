c48196e9df40ceb1445d0f1dd5ade2472203c860
Zingerella <jamil.haidarahmad@outlook.com>
2021-03-24 07:57:20 +0200

    oh god

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,14 @@
 pass
-# if __name__ == "__main__":
-#         print('In wsgi')
-#         print('In wsgi: Import done. running..')
-#         app.run()
-#         print('In wsgi: ran flask')
-#         print('Done running')
+# from app.main import app
+# print('Starting wsgi')
+# import jamilbot
+# from threading import Thread
+# mbot=Thread(jamilbot.weird_run).start()
+# app.run()
+# mbot.join()
+# # if __name__ == "__main__":
+# #         print('In wsgi')
+# #         print('In wsgi: Import done. running..')
+# #         app.run()
+# #         print('In wsgi: ran flask')
+# #         print('Done running')
